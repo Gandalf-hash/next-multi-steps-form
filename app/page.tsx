e36f04baf23e3dc43d28752b5538f3a768aaa3d9
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-24 11:16:26 +0200

    fix: refactor the overall code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 import { Metadata } from "next";
-import Home from "./home/page";
 
 export const metadata: Metadata = {
   title: "Form",
 };
 export default function page() {
-  return (
-    <main>
-      <Home />
-    </main>
-  );
+  return <main></main>;
 }
--- app/ui/page-wrapper/page-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AppPageWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="w-full p-6 flex justify-center items-center min-h-screen lg:max-w-full">
-      {children}
-    </div>
-  );
-};
