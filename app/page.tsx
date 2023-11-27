1afb561040735898af993a821decf68226a48841
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-27 16:07:04 +0200

    feat: add first step

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import { Metadata } from "next";
+"use client";
+import React, { useState } from "react";
+import { LeftAlignedImage } from "./ui/frame/left-aligned-image";
+import FirstStep from "./step1/first-step";
+import SecondPage from "./step2/page";
+import { AppPrimaryButton } from "./ui/button/primary-button";
 
-export const metadata: Metadata = {
-  title: "Form",
+const App = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  return (
+    <main className="w-full max-w-full flex items-center min-h-screen justify-center">
+      <div className="rounded-xl gap-12 lg:gap-20 p-6 w-full flex flex-row max-w-4xl xl:max-w-5xl bg-white h-full">
+        <LeftAlignedImage />
+        <div className="mt-12">
+          {currentStep === 1 && <FirstStep setCurrentStep={setCurrentStep} />}
+          {currentStep === 2 && <SecondPage />}
+        </div>
+      </div>
+    </main>
+  );
 };
-export default function page() {
-  return <main></main>;
-}
+
+export default App;
