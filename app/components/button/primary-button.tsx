ef278ae973f974de793d0341ab25266c5dd4ba0d
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-28 09:18:57 +0200

    chore: destructure classes

---
+++ app/components/button/primary-button.tsx
@@ -0,0 +1,28 @@
+import { clsx } from "clsx";
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+  onClick?: () => void;
+  children?: ReactNode;
+  disabled?: boolean;
+}
+
+export const AppPrimaryButton = ({
+  onClick,
+  disabled,
+  children,
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(
+        "text-white text-lg focus:outline-none focus:ring active:bg-blue-950",
+        "rounded-md flex hover:bg-olympian-blue items-center px-4 py-2  bg-downriver",
+        { "cursor-not-allowed": disabled === true }
+      )}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children || "Next Steps"}
+    </button>
+  );
+};
