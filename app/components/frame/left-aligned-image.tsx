594e94ba24ce1a9e6fa1155ea3dcf14f30b18234
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-28 12:33:56 +0200

    feat: add a step component

--- app/components/frame/left-aligned-image.tsx
+++ app/components/frame/left-aligned-image.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import { NumberRadioButton } from "../steps/steps";
 
-export const LeftAlignedImage = () => {
+interface LeftAlignedImageProps {
+  currentStep: number;
+}
+
+export const LeftAlignedImage = ({ currentStep }: LeftAlignedImageProps) => {
+  const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+
+  const handleNumberChange = (number: number) => {
+    setSelectedNumber(number);
+  };
   return (
-    <div className="hidden md:block h-full w-full max-w-xs">
+    <div className="relative hidden md:block h-full w-full max-w-xs">
+      <div className="absolute left-0 top-12 flex flex-col gap-6">
+        <NumberRadioButton
+          step="step 1"
+          section="your info"
+          number={1}
+          checked={currentStep === 1}
+        />
+        <NumberRadioButton
+          step="step 2"
+          section="select plan"
+          number={2}
+          checked={currentStep === 2}
+        />
+        <NumberRadioButton
+          step="step 3"
+          section="add ons"
+          number={3}
+          checked={currentStep === 3}
+        />
+        <NumberRadioButton
+          step="step 4"
+          section="summary"
+          number={4}
+          checked={currentStep === 4}
+        />
+      </div>
+
       <div>
         <Image
           className="object-cover object-left hidden md:block"
