8f8d949814894c334c02d3af261035699127fcef
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-29 15:33:13 +0200

    feat: back button

--- app/components/button/back-button.tsx
+++ app/components/button/back-button.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const AppBackButton = () => {
+interface Props {
+  onClick: () => void;
+}
+
+export const AppBackButton = ({ onClick }: Props) => {
   return (
-    <div className="">
-      <span>Title</span>
+    <div className="text-gray-400">
+      <button
+        onClick={onClick}
+        className="font-bold text-lg hover:text-downriver-blue"
+      >
+        Go Back
+      </button>
     </div>
   );
 };
